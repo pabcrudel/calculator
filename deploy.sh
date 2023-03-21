d8e36a6feab8f928ada859ea970ec9d5d80d2a01
pabcrudel <pabcrudel@gmail.com>
2023-03-21 10:02:29 +0100

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,9 @@ commit_hash=$(echo $git_log | awk '{print $2}')
 # Get current GitHub Repository URL
 repo_url=$(git remote show origin | grep Push | awk '{print $3}')
 
+# Change the remote url to lowercase
+repo_url=$(echo "$repo_url" | tr '[:upper:]' '[:lower:]')
+
 # Build
 npm run build
 
@@ -28,5 +31,8 @@ git add -A
 git commit --allow-empty -m "Deploy (commit: $commit_hash)"
 
 branch=$(git rev-parse --abbrev-ref $(git symbolic-ref HEAD))
+
+# Set the current branch as an upstream branch
+git push --set-upstream origin main
 
 git push -f $repo_url $branch:gh-pages -v
