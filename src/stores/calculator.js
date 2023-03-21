6ea49e74de7dadb45a526fe01f7ec497feaeb73b
pabcrudel <pabcrudel@gmail.com>
2023-03-21 12:19:04 +0100

    Split main function in small functions

--- src/stores/calculator.js
+++ src/stores/calculator.js
@@ -4,70 +4,93 @@ export const useCalculator = defineStore('Calculator', {
     state: () => ({
         current: {
             operation: "0",
-            result: 0
+            result: "0"
         },
         last: {
             operation: "0",
-            result: 0
+            operator: "",
+            result: "0"
         },
         buffer: {
             operation: "0",
-            result: 0
+            result: "0"
         },
-        saveOperation: false,
+        isSaved: false,
     }),
     actions: {
-        sendItem(item){
-            /* Checking if the item is a number. If it is, it adds it to the number. */
-            if (!isNaN(item)) {
-                this.current.operation === "0" ? this.current.operation = item : this.current.operation += item;
-                
-                this.current.result = eval(this.current.operation)
-            }
-            /* Checking if the item is backspace and if the number has something. If it is, it will slice the number. */
-            else if (item === "backspace") {
-                const opLength = this.current.operation.length;
+        handleInput(item){
+            switch (true) {
+                case /^\d+(\.\d+)?$/.test(item):
+                    this.setOperation(item)
+                    break;
+                case item === "backspace":
+                    this.saveOperation()
+                    this.backspace()
+                    break;
+                case item === "C":
+                    this.$reset()
+                    break;
+                case item === "=":
+                    this.setBuffer()
+                    break;
+                case /[+\-*/%.]/.test(item):
+                    this.saveOperation()
+                    this.updateOperation(item)
+                    break;
+            };
+        },
+        setOperation(item){
+            this.current.operation === "0" ? this.current.operation = item : this.current.operation += item;
 
-                if (opLength > 0) {
-                    /* Checking if the last character of the string is a space. If it is, it will slice
-                    the string by 3. If it is not, it will slice the string by 1. */
-                    this.current.operation = 
-                    this.current.operation.charAt(opLength - 1) === " " ? 
-                    this.current.operation.slice(0, -3) : 
-                    this.current.operation.slice(0, -1);
+            this.last.operator = "";
 
-                    this.current.result = eval(this.last.operation);
-                }
+            this.calc(this.current.operation)
+        },
+        calc(operation){
+            this.current.result = `${eval(operation)}`
+        },
+        backspace(){
+            const opLength = this.current.operation.length;
+
+            if (opLength > 0) {
+                /* Checking if the last character of the string is a space. If it is, it will slice
+                the string by 3. If it is not, it will slice the string by 1. */
+                this.current.operation = 
+                this.current.operation.charAt(opLength - 1) === " " ? 
+                this.current.operation.slice(0, -3) : 
+                this.current.operation.slice(0, -1);
+
+                this.calc(this.last.operation)
             }
+        },
+        // clear(item){
+
+        // },
+        setBuffer(){
+            this.buffer.operation = this.current.operation;
+            this.buffer.result = this.current.result;
             
-            /* Resetting the store. */
-            else if (item === "C") {this.$reset()}
-
-            /* Setting the last operation to the current operation and then resetting the current
-            operation. */
-            else if (item === "=") {
-                this.buffer.operation = this.current.operation;
-                this.buffer.result = this.current.result;
-                
-                this.saveOperation = true;
-            }
-            
-            /* Adding the item to the operation and then evaluating the operation. */
-            else {
-                if (this.saveOperation) {
-                    this.saveOperation = false;
-
-                    this.last.operation = this.current.operation;
-                    this.last.result = this.current.result;
-
-                    this.current.operation = this.current.result;
-                }
-
+            this.isSaved = true;
+        },
+        saveOperation(){
+            if (this.isSaved) {
+                this.isSaved = false;
+    
+                this.last.operation = this.current.operation;
+                this.last.result = this.current.result;
+    
+                this.current.operation = this.current.result;
+            };
+        },
+        updateOperation(item){
+            if (this.last.operator != item) {
                 this.current.operation += 
                 item === "." ?
                 item :
                 ` ${item} `;
+    
+                this.last.operator = item;
             };
-        }
+        },
     },
 })
